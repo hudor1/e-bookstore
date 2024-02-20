425597a3dea6148ff889e5d204879344361807b1
hudor1 <147792241+hudor1@users.noreply.github.com>
2024-02-20 13:41:40 +0200

    Update ebookstore.py

--- ebookstore.py
+++ ebookstore.py
@@ -1,12 +1,4 @@
 import sqlite3
-
-# The program allows the user to:
-# --Add new books to the database
-# --update books to the database
-# --delete books from the database
-# --search the database to find a specific book
-# I got help from realpython.com for writing docstrings.
-
                
 def exit():        
     """Closes the database and shuts down the program."""            
